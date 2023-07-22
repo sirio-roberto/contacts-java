d93116fbc9c9f242784039026813512b24906599
Sirio <sirioroberto1@hotmail.com>
2023-07-22 13:10:30 -0300

    Added getter and setter to Contact.phone field. The setter has a validation to accept only a specific pattern

--- contacts/Contact.java
+++ contacts/Contact.java
@@ -1,5 +1,7 @@
 package contacts;
 
+import java.util.Arrays;
+
 public class Contact {
     private String name;
     private String surname;
@@ -9,5 +11,44 @@ public class Contact {
         this.name = name;
         this.surname = surname;
         this.phone = phone;
+    }
+
+    public Contact() {}
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        if (isValidPhoneNumber(phone)) {
+            this.phone = phone;
+        } else {
+            System.out.println("Wrong number format!");
+        }
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            return false;
+        }
+        String[] groups = phoneNumber.split("[ -]");
+        if (groups.length < 4) {
+            return false;
+        }
+        if (groups[0].matches("^\\+?\\(.+\\)$") && groups[1].matches("^\\(.+\\)$")) {
+            return false;
+        }
+        if (!groups[0].matches("(?i)^\\+?\\(?[a-z0-9]+\\)?$")) {
+            return false;
+        }
+        if (!groups[1].matches("(?i)^\\(?[a-z0-9]{2,}\\)?$")) {
+            return false;
+        }
+        for (int i = 2; i < groups.length; i++) {
+            if (!groups[i].matches("(?i)^?[a-z0-9]{2,}?$")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
